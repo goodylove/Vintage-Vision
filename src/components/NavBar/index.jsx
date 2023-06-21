08b281869a50949156617fe6a3c81747f5ac33b0
goodness <goodyc474@gmail.com>
2023-06-21 11:35:01 +0100

    ft-#2:add vercel json file

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Typography, Avatar, Input } from '@material-tailwind/react';
 import { Link } from 'react-router-dom';
 import { BiCart } from 'react-icons/bi';
-import { BsSearch } from 'react-icons/bs';
+
 import { MdOutlineFavoriteBorder, MdOutlineFavorite } from 'react-icons/md';
 import { RxHamburgerMenu } from 'react-icons/rx';
 import { AiOutlineClose } from 'react-icons/ai';
@@ -23,35 +23,11 @@ const Nav = () => {
   const [mobileMenu, setmobileMenu] = useState(false);
   return (
     <main className="w-full  flex-col fixed top-0 cursor-pointer z-50">
-      {/* {search && (
-        <div className="w-52 absolute left-[30%] top-[6rem] ease-in-out  scale-100">
-          <Input
-            label="search"
-            color="blue-gray"
-            className=" "
-            variant="outlined"
-          />
-        </div>
-      )} */}
-      <nav className="flex justify-between md:px-6 md:py-5 items-center bg-[#0D1C1E]  px-1 py-2 ">
+      <nav className="flex justify-between items-center bg-[#0D1C1E]  px-1 py-2 ">
         <Typography className="text-[15px] text-[#D0D0CF] md:text-[20px]  md:font-[600]">
           VINTAGEVISION
         </Typography>
-        <ul className="md:hidden  justify-around gap-8 items-center hidden text-[#D0D0CF] sm:hidden lg:flex">
-          {myLinks.map((item, index) => {
-            return (
-              <li className="" key={index}>
-                <Link
-                  to={item.link}
-                  className="uppercase"
-                  onClick={() => disptach(pageProduct(item.name))}
-                >
-                  {item.name}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
+
         <div className="flex gap-2 items-center  text-[#D0D0CF] flex-row">
           <Link to="/whishlist">
             <div className="relative">
@@ -66,7 +42,6 @@ const Nav = () => {
             </div>
           </Link>
 
-          {/* <BsSearch onClick={() => setSearch((prev) => !prev)} /> */}
           <Link to="/cart">
             <div className="relative">
               <BiCart className="text-[25px]" />
@@ -84,15 +59,6 @@ const Nav = () => {
           </span>
         </div>
         <div className="flex md:hidden text-[#D0D0CF]">
-          {/* <Hamburger
-            onToggle={(toggleMenu) => {
-              if (toggleMenu) {
-                setToggleMenu(true);
-              } else {
-                setToggleMenu(false);
-              }
-            }}
-          /> */}
           {toggleMenu ? (
             <AiOutlineClose
               className="text-[45px] pr-3 text-white"
